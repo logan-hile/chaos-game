7cdc8e4594fffd8324e46f8ba8b7e31ca9d49cd3
Logan Hile <loganhile.business@gmail.com>
2025-12-03 13:51:12 -0600

    Something with VS code is broken

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 running = True
 
-def findArea(pointA, pointB, pointC):
+def findArea(vertices):
+    pA, pB, pC = vertices # p = Point (pointA, pointB, pointC)
     # Using Heron's formula
-    x1, x2, x3 = pointA[0], pointB[0], pointC[0]
-    y1, y2, y3 = pointA[1], pointB[1], pointC[1]
+    x1, x2, x3 = pA[0], pB[0], pC[0]
+    y1, y2, y3 = pA[1], pB[1], pC[1]
 
     # Using the distance formula to calculate length of each side
     ab = math.sqrt((x2-x1)**2 + (y2-y1)**2) # Between A and B
@@ -22,21 +23,44 @@ def findArea(pointA, pointB, pointC):
 
     return area
 
-def pointInTriangle(point, triangleVertices):
-    a, b, c = triangleVertices
+def findMiddlePoint(pA, pB):
+    x1, x2 = pA[0], pB[0]
+    y1, y2 = pA[1], pB[1]
+    
+    xCoord = abs(x2-x1)
+    yCoord = abs(y2-y1)
 
-    if findArea(point, a, b) + findArea(point, b, c) + findArea(point, a, c) <= findArea(a, b, c):
+    print(xCoord, yCoord)
+    
+    return(xCoord, yCoord)
+
+firstPointCoordinates = (random.randint(250, 750), random.randint(250, 500)) # Starts with random coordinates to start
+
+basePoints = {"A":(250,500), "B":(500,250), "C":(750,500)} # Main three points creating the base trinagle
+baseArea = findArea(basePoints.values())
+
+lastPlottedCoordinates = (0,0)
+
+def pointInTriangle(point, vertices):
+    pA, pB, pC = vertices
+
+    if findArea((point, pA, pB)) + findArea((point, pB, pC)) + findArea((point, pA, pC)) <= baseArea:
         return point
     
     return False
 
-pointCoordinates = (random.randint(250, 750), random.randint(250, 500)) # Starts with random coordinates to start
+def pickRandomVertice():
+    rand = random.randint(0, 2)
 
-basePoints = {"A":(250,500), "B":(500,250), "C":(750,500)} # Main three points creating the base trinagle
-baseArea = findArea(basePoints["A"], basePoints["B"], basePoints["C"])
+    if rand == 0:
+        return("A")
+    elif rand == 1:
+        return("B")
+    
+    return("C")
 
-while pointInTriangle(pointCoordinates, (basePoints["A"], basePoints["B"], basePoints["C"])) == False:
-    pointCoordinates = (random.randint(250, 750), random.randint(250, 500))
+while not pointInTriangle(firstPointCoordinates, basePoints.values()):
+    firstPointCoordinates = (random.randint(250, 750), random.randint(250, 500)) # Get new random coordinates
 
 while running:
     for event in pygame.event.get():
@@ -48,7 +72,14 @@ while running:
     for point in basePoints:
         pygame.draw.circle(screen, "black", basePoints[point], 5) # Visually plot each point
 
-    pygame.draw.circle(screen, "deeppink2", pointCoordinates, 5) # Visually plot each point
+    pygame.draw.circle(screen, "deeppink2", firstPointCoordinates, 5)
+
+    for i in range(0, 1000):
+        baseVertice = pickRandomVertice()
+        midPoint = findMiddlePoint(lastPlottedCoordinates, basePoints[baseVertice])
+
+        pygame.draw.circle(screen, "green", midPoint, 5)
+
 
     pygame.display.flip() # Display to screen
 
